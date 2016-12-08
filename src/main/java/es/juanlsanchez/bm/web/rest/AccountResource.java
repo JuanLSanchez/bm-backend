13c43e171cf6da81a44b67b09e77e0ef6f40277f
Juan L. Sánchez <juanlu-28a@hotmail.com>
2016-12-08 20:13:40 +0100

    feat: create test to account

---
+++ src/main/java/es/juanlsanchez/bm/web/rest/AccountResource.java
@@ -0,0 +1,14 @@
+package es.juanlsanchez.bm.web.rest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import es.juanlsanchez.bm.config.Constants;
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@RequestMapping(Constants.START_URL_API + "/account")
+@Slf4j
+public class AccountResource {
+
+}
